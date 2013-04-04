096348634930850338d8698b257eefa403077f06
Chris D'Angelo <cd2665@columbia.edu>
2013-04-04 01:08:57 -0400

    Added Cast and Crew A-Z Page

--- src/com/moviesdb/CastAndCrew.java
+++ src/com/moviesdb/CastAndCrew.java
@@ -2,12 +2,17 @@ package com.moviesdb;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import oracle.jdbc.pool.OracleDataSource;
 
 /**
  * Servlet implementation class CastAndCrew
@@ -30,14 +35,58 @@ public class CastAndCrew extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        
         String loggedinemail = request.getParameter("loggedinemail");
 	
-        // print logged in name
-        out.println("<p align=\"right\">Logged in: " + loggedinemail + "</p>");     
+        String dbUser = "cd2665"; // enter your username here
+        String dbPassword = "movies"; // enter your password here
+
+        try {
+                OracleDataSource ods = new oracle.jdbc.pool.OracleDataSource();
+                ods.setURL("jdbc:oracle:thin:@//w4111g.cs.columbia.edu:1521/ADB");
+                ods.setUser(dbUser);
+                ods.setPassword(dbPassword);
         
-        out.println("<h1>Cast and Crew A-Z </h1>");
-        
+                Connection conn = ods.getConnection();
+
+                String query = new String();
+                Statement s = conn.createStatement();
+                
+                query = "select cid, firstname, lastname " +
+                		"from castcrew order by lastname asc";
+                ResultSet r = s.executeQuery(query);
+    
+                // print logged in name
+		        out.println("<p align=\"right\">Logged in: " + loggedinemail + "</p>");     
+		        
+		        out.println("<h1>Cast and Crew A-Z </h1>");
+                out.println("<table border=\"1\">");
+                out.println("<tr>" +
+							"<th>First Name</th>" +
+							"<th>Last Name</th>" +
+							"<th>Link to Cast/Crew Details</th>" +
+							"</tr>");
+                while(r.next()) {
+                    out.println("<tr>");
+                    out.println("<td>" + r.getString(2) + "</td>");
+                    out.println("<td>" + r.getString(3) + "</td>");
+                    out.println("<td><form action='castandcrewdetails' method='get' enctype='text/plain'>" + 
+                 		   	   "<input type='submit' name='cid' value=" +
+                 		       "'" + r.getString(1) + "'/> " +
+                 		   	   "<input type=\"hidden\" value=\"" + loggedinemail + "\" name=\"loggedinemail\">" +
+                 		       "</form></td>");
+                    out.println("</tr>");                           
+                }
+                out.println("</table>");
+		        r.close();
+                s.close();
+                conn.close();
+
+        }
+        catch (Exception e) {
+                out.println("The database could not be accessed.<br>");
+                out.println("More information is available as follows:<br>");
+                e.printStackTrace(out);
+        }
 	}
 
 	/**
